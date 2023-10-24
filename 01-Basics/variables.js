9701cc0b5b075de605aa065f189ea933a5c25331
Kupendra V <93828746+kupendrav@users.noreply.github.com>
2023-10-24 16:32:25 +0000

    variables

---
+++ 01-Basics/variables.js
@@ -0,0 +1,9 @@
+const accoundId = 155;
+let accountEmail = 'kupendrav99@gmail.com'
+var accountPassword = '12345'
+accountCity = 'banglore'
+
+// accoundId = 34
+// const cannot be changed
+
+console.log(accoundId);
